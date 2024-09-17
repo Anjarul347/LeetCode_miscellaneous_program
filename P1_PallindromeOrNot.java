8650b2e069a9cb0edb9d0d23e4b2b8bcdad474fb
Anjarul347 <skanjarulhossain347@gmail.com>
2024-09-17 23:16:40 +0530

    Pallindrome completed

--- P1_PallindromeOrNot.java
+++ P1_PallindromeOrNot.java
@@ -9,7 +9,21 @@ import java.util.Scanner;
 public class P1_PallindromeOrNot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number to cheak wheather it is pallindrome or not:-");
+        int num=sc.nextInt();
+        int r,pallin=0,p=num;
 
+        while(num>0){
+            r=num%10;
+            pallin=(pallin*10)+r;
+            num/=10;
+        }
+        if(p==pallin){
+            System.out.println("The given number is Pallindrome");
+        }
+        else{
+            System.out.println("The given number is not Pallindrome");
+        }
         
     }
 }
