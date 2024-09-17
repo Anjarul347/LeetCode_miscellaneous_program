38664b4b6e47a921410c9ecbed3166ce7d224350
Anjarul347 <skanjarulhossain347@gmail.com>
2024-09-17 23:05:46 +0530

    first commit

---
+++ P1_PallindromeOrNot.java
@@ -0,0 +1,15 @@
+/*
+9. Palindrome Number
+
+Given an integer x, return true if x is a 
+palindrome, and false otherwise.
+ */
+import java.util.Scanner;
+
+public class P1_PallindromeOrNot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+    }
+}
